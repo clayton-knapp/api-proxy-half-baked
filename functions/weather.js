24921f5e987196c179c747cddbf253edd008d3cb
Clayton Knapp <csjknapp@gmail.com>
2022-02-15 16:54:39 -0800

    weather front end built

--- functions/weather.js
+++ functions/weather.js
@@ -1,17 +1,26 @@
-const request = require('superagent');
+const request = require('node-fetch');
 
 require('dotenv').config();
 
-exports.handler = async (event, context) => {
+exports.handler = async (e) => {
   try {
     // grab the city, state, and country from the request's query parameters
     // here is an example from the netlify docs:
     // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
+    const { city, state, country } = e.queryStringParameters;
+    // console.log(city, state, country);
+
 
     // tragicly, we cannot just pass the city name to this API. it wants a latitude and longitude for the weather
-    // consult the yelp docs to figure out how to use a city, state, and country to make a request and get the latitude and longitude
+    // consult the weather docs to figure out how to use a city, state, and country to make a request and get the latitude and longitude
     // https://openweathermap.org/api/geocoding-api
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=portland,or,usa&limit=1&appid=6f8655cd91af43cddf2c32d6c9dc91fa`);
 
+    console.log('RESPONSE', response);
+
+    const json = await response.json();
+
+    
     // once you have gotten the lat/lon using the geocoding api, use the lat/lon to get the weather. Consult the docs below:
     // https://openweathermap.org/api/one-call-api
 
@@ -19,8 +28,9 @@ exports.handler = async (event, context) => {
     return { 
       statusCode: 200, 
     // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the weather data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify({}),
+      body: JSON.stringify(json),
     };
+
   } catch (error) {
     console.log(error);
     return {
