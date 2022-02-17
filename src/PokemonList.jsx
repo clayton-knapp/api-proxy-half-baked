13df30ef966ffa9aacdabe0db93b366cf09987e0
Clayton Knapp <csjknapp@gmail.com>
2022-02-17 15:30:38 -0800

    passing tests after dani helped add special code for netlify

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function PokemonList({ pokemonArr }) {
   // console.log(pokemonArr);
   return (
-    <div className='pokemon-list'>
+    <div className='pokemon-list' title='pokemon-list'>
       {
         pokemonArr.map((pokemon, i)=> 
-          <div key={pokemon + i} className='pokemon-item'>
+          <div key={pokemon + i} className='pokemon-item' title='pokemon-item'>
             <h3>{pokemon.pokemon}</h3>
             {/* <h4>Type: {pokemon.type_1}</h4> */}
             <img src={pokemon.url_image}></img>
